3329daabadc736d927d762808bf1062d88ab40f0
Matondo <matondojoaokitemoco@outlook.pt>
2024-10-01 13:35:54 +0100

    Config app

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Core\Example;
+
+$ex = new Example();
+echo $ex->teste();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'matondo/php-tdd',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1b60f413295c692eee2650797ad8554b8e0b49e3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'matondo/php-tdd' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1b60f413295c692eee2650797ad8554b8e0b49e3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
